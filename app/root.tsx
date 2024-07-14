afbe8b365a6f7beed936166e2c2e29e8bfee380c
Ivan Li <ivanli2048@gmail.com>
2024-07-14 14:58:38 +0800

    feat: i18n support.

--- app/root.tsx
+++ app/root.tsx
@@ -4,13 +4,28 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  json,
+  useLoaderData,
 } from "@remix-run/react";
 import "./tailwind.css";
+import type { LoaderFunctionArgs } from "@remix-run/node";
 import type { ReactNode } from "react";
+import i18next from "./i18next.server";
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const locale = await i18next.getLocale(request);
+  return json({ locale });
+}
+
+export const handle = {
+  i18n: "common",
+};
 
 export function Layout({ children }: { children: ReactNode }) {
+  const { locale } = useLoaderData<typeof loader>();
+
   return (
-    <html lang="en">
+    <html lang={locale}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
